9c7922f0c192e056d928cdeedf6d520393951c10
AasifNazir786 <aasif.n.s786@gmail.com>
2024-12-09 11:37:50 +0530

    Implemented Booking and Customer Entities

---
+++ src/main/java/com/example/hotel_booking_system/models/Booking.java
@@ -0,0 +1,92 @@
+package com.example.hotel_booking_system.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate arrivalDate;
+    private LocalDate departureDate;
+    private int daysStayed;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public Booking() {
+    }
+
+    public Booking(Long id, LocalDate arrivalDate, LocalDate departureDate, int daysStayed, Room room,
+            Customer customer) {
+        this.id = id;
+        this.arrivalDate = arrivalDate;
+        this.departureDate = departureDate;
+        this.daysStayed = daysStayed;
+        this.room = room;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public int getDaysStayed() {
+        return daysStayed;
+    }
+
+    public void setDaysStayed(int daysStayed) {
+        this.daysStayed = daysStayed;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    
+}
